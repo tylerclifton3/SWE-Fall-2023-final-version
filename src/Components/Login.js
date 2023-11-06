af9ed0c60badf42241f9986494f3d5c070a39ccd
tylerclifton3 <100748044+tylerclifton3@users.noreply.github.com>
2023-11-06 12:42:35 -0600

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,52 @@
 import React, { useState } from "react";
 import "../styles/LoginPage.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import axios from 'axios';
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
 
-  const handleLogin = () => {
-    // Handle your login logic here, e.g., API call, form validation, etc.
-    console.log("Logging in with", email, password);
+  const handleCreateUser = async () => {
+    try {
+      const response = await axios.post(
+        'https://wdlnvxccyg.execute-api.us-east-1.amazonaws.com/beta/',
+        {
+          username: email,  // Set the username to the email
+          password: 'password' 
+        }
+      );
+
+      console.log('User created:', response.data.message);
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      // Perform the login request to your backend API
+      const response = await axios.post(
+        'https://wdlnvxccyg.execute-api.us-east-1.amazonaws.com/beta/', 
+        {
+          username: email,
+          password: password
+        }
+      );
+
+      if (response.status === 200) {
+        // Login successful
+        console.log('Login successful');
+        // Redirect to the home page or the desired destination
+        history.push('/');
+      } else {
+        // Handle other response statuses if needed
+      }
+    } catch (error) {
+      // Handle login error
+      console.error('Login error:', error);
+    }
   };
 
   return (
@@ -28,9 +66,7 @@ const LoginPage = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={handleLogin}>Login</button>
-        {/* <a href="#forgot-password" className="forgot-password-link">
-          Forgot Password?
-        </a> */}
+        <button onClick={handleCreateUser}>Create User</button>
         <div style={{ marginTop: 20 }}>
           <span>Don't have an account? </span>
           <Link to="/register">Register</Link>
